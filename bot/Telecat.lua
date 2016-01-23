4f8272f490ddeb9d2860acb4924ebd5841ba6107
badboyam <pouya001111@gmail.com>
2016-01-23 08:17:30 -0800

    Update Telecat.lua

--- bot/Telecat.lua
+++ bot/Telecat.lua
@@ -241,7 +241,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {160553465},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
